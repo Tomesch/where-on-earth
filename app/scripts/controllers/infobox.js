f30673dbe8fa5ae12d577d0582191821525ff1a3
Joris Girardot <joris.girardot@gmail.com>
2014-04-13 15:45:46 +0200

    rage retour

--- app/scripts/controllers/infobox.js
+++ app/scripts/controllers/infobox.js
@@ -1,19 +1,47 @@
 'use strict';
 
 angular.module('woeApp')
-  .controller('InfoboxCtrl', function ($scope, $http, socket) {
-  	$scope.location = [];
-  	$scope.display = 'display: none';
-  	var timer;
-  	socket.on('information', function(data){
-  		console.log(data);
-  		$scope.location = data.location;
-  		$scope.display = 'display: block';
+.controller('InfoboxCtrl', function ($scope, $http, socket) {
+	$scope.location = [];
+	$scope.display = 'display: none';
+	$scope.desc = '';
+	$scope.site = '';
 
-  		timer = setTimeout(function() {
-  			console.log("interval")
-  			$scope.display = 'display:none';
-  			$scope.$apply();
-  		}, 10000);
-    });
-  });
+	var timer;
+	socket.on('information', function(data){
+		console.log(data);
+		$scope.location = data.location;
+		$scope.display = 'display: block';
+
+		$scope.desc = replaceHtml(data.location.short_description[0]);
+		$scope.site = replaceHtml(data.location.site[0]);
+
+		timer = setTimeout(function() {
+			console.log("interval")
+			$scope.display = 'display:none';
+			$scope.$apply();
+		}, 10000);
+
+
+		function replaceHtml(text) {
+
+			text = text.replace('<p>', '');
+			text = text.replace('</p>','');
+
+			text = text.replace('<em>', '');
+			text = text.replace('</em>','');
+
+			text = text.replace('&eacute;', 'é');
+			text = text.replace('&amp;', '&');
+			text = text.replace('&ndash;', '-');
+			text = text.replace('<span>;', '');
+			text = text.replace('</span>;', '');
+			text = text.replace('&nbsp;', ' ');
+			text = text.replace('&ldquo;', '"');
+			text = text.replace('&rdquo;', '"');
+
+
+			return text;
+		}
+	});
+});
